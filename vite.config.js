6e6ff53e2f03742fb01c0f69fd19ff83b102f017
aditya kale <adityakale@adityas-MacBook-Air.local>
2025-10-07 02:50:00 +0530

    Initial Setup Complete

---
+++ vite.config.js
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    base: '/', // ✅ fix: use root for vercel deployment
+    plugins: [react()],
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        // Split the swiper plugin library into a separate chunk
+                        if (id.includes('swiper')) return 'swiper'
+                        return
+                    }
+                },
+            },
+        },
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                silenceDeprecations: [
+                    'mixed-decls',
+                    'color-functions',
+                    'global-builtin',
+                    'import',
+                ],
+            },
+        },
+    },
+})
